9ea75a5dabe1a28c272a4d7f9b446ad68b78695a
Jeffrey Hutchins <jiffy@Jeffreys-MacBook-Air.local>
2019-09-04 15:23:47 -0500

    3.6_functions_exercies.sql

--- 3.6_functions_exercises.sql
+++ 3.6_functions_exercises.sql
@@ -3,18 +3,19 @@
 use employees;
 
 -- Update your queries for employuees whos names start and end with 'E'.  Use concat() to combien their first and last names together as a single column named full_name.
-SELECT CONCAT(first_name,' ',last_name) AS full_name FROM employees WHERE last_name LIKE ('E%') OR last_name LIKE ('%E');
+SELECT CONCAT(first_name,' ',last_name) AS full_name FROM employees WHERE CONCAT(first_name,' ',last_name) like 'e%e';
 
 -- Convert the names produced in previous query to all uppercase.
-SELECT UPPER(CONCAT(first_name,' ',last_name)) AS full_name FROM employees WHERE last_name LIKE ('E%') OR last_name LIKE ('%E');
+SELECT UPPER(CONCAT(first_name,' ',last_name)) AS full_name FROM employees WHERE CONCAT(first_name,' ',last_name) like 'e%e';
 
 -- For query of employees born on Christmas and hired in the 90s, use datediff() to find how many days they have been working at the company.
-SELECT datediff(curdate(), hire_date) from employees
+SELECT datediff(curdate(), hire_date) as days_with_company, first_name, last_name from employees
 WHERE hire_date>'1990' AND hire_date <'2000' 
-AND birth_date LIKE '%-12-25';
+AND birth_date LIKE '%-12-25'
+order by days_with_company DESC;
 
 -- Find the smallest and largest salary from the salaries table.
-SELECT MAX(salary), MIN(salary)
+SELECT MAX(salary) as 'largest_salary', MIN(salary) as 'smallest_salary', avg(salary) as 'average_salary'
 FROM salaries;
 
 -- Generate a username for all of the employees.
@@ -25,5 +26,7 @@ substring(last_name,1,4),
  '_', 
  substring(birth_date,-5,2), 
  substring(birth_date,3,2))), 
- first_name, last_name, 
- birth_date from employees;
+ first_name, 
+ last_name, 
+ birth_date 
+ from employees;
