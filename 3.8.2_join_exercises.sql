8e4ac5a1f6d606cf89838bbd0ebed9fc4ff574cd
Jeffrey Hutchins <jiffy@Jeffreys-MacBook-Air.local>
2019-09-05 16:48:36 -0500

    3.8.2

--- 3.8.2_join_exercises.sql
+++ 3.8.2_join_exercises.sql
@@ -84,7 +84,7 @@ where dept_emp.to_date = '9999-01-01'
 group by departments.dept_name
 order by dept_no;
 
--- Which department has the highest average salary?
+-- Which department has the highest average salary? Anytime you use an aggregate function you need a "group by"
 select departments.dept_name as dept_name, avg(salaries.salary) as average_salary
 from departments
 join dept_emp
@@ -93,5 +93,47 @@ join salaries
 on dept_emp.emp_no = salaries.emp_no
 where salaries.to_date = '9999-01-01'
 group by departments.dept_name
-having average_salary > 82000
+-- having average_salary > 82000
 order by dept_name DESC
+limit 1;
+
+-- Who is the highest paid employee in the Marketing department?
+select employees.first_name, employees.last_name
+from employees
+join dept_emp
+on dept_emp.emp_no = employees.emp_no
+join departments
+on departments.dept_no = dept_emp.dept_no
+join salaries
+on salaries.emp_no = employees.emp_no
+where salaries.to_date = '9999-01-01' and dept_name = 'Marketing'
+group by employees.first_name, employees.last_name, salaries.salary, departments.dept_name
+order by salary DESC
+limit 1;
+
+-- Which current department manager has the highest salary?
+select employees.first_name, employees.last_name, salaries.salary, departments.dept_name as dept_name
+from departments
+join dept_manager
+on dept_manager.dept_no = departments.dept_no
+join employees
+on employees.emp_no = dept_manager.emp_no
+join salaries
+on salaries.emp_no = employees.emp_no
+where salaries.to_date = '9999-01-01' and dept_manager.to_date = '9999-01-01'
+order by salary desc
+limit 1;
+
+-- BONUS
+-- Find the names of all current employees, their department name, and their current manager's name.
+select concat(employees.first_name,' ',employees.last_name) as Employee_Name, departments.dept_name as Department_Name, concat(employees.first_name,' ',employees.last_name) as Manager_Name
+from employees
+join dept_emp
+on dept_emp.emp_no = employees.emp_no
+join departments
+on departments.dept_no = dept_emp.dept_no
+join dept_manager
+on departments.dept_no = dept_manager.dept_no
+where dept_manager.to_date = '9999-01-01'
+group by employees.first_name, employees.last_name, Department_Name, Manager_Name
+order by Department_Name;
