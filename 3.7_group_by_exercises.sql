0a6d3d16212e6775aeefe34e1640d07e8b8680f9
Jeffrey Hutchins <jiffy@Jeffreys-MacBook-Air.local>
2019-09-05 09:37:03 -0500

    3.7_group_by_exercises

--- 3.7_group_by_exercises.sql
+++ 3.7_group_by_exercises.sql
@@ -7,28 +7,30 @@ select distinct title from titles
 select * from employees;
 
 -- Update query for employees whose last name starts and ends with 'E' to find just the unique last names that start and end with 'E'.
-SELECT distinct last_name  
+SELECT last_name  
 FROM employees 
 WHERE last_name
 like 'e%e'
 GROUP BY last_name;
 
 -- Update previous query to find unique combos of first and last name.
-SELECT distinct last_name, first_name 
+SELECT last_name, first_name 
 FROM employees 
 WHERE last_name
-like 'e%e';
+like 'e%e'
+group by last_name,first_name;
 
 -- Find unique last names with a 'q' but not'qu'.
-SELECT distinct last_name 
+SELECT last_name 
 FROM employees 
 WHERE last_name 
 LIKE ('%q%') 
 AND last_name 
-NOT LIKE ('%qu%');
+NOT LIKE ('%qu%')
+group by last_name;
 
 -- Add a COUNT() to results and use ORDER BY to make it easier to find employees whose unusual name is shared with others.
-SELECT distinct last_name, count(*)
+SELECT last_name, count(*)
 FROM employees 
 WHERE last_name 
 LIKE ('%q%') 
@@ -44,7 +46,7 @@ WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya')
 GROUP BY gender
 ORDER BY count(*) DESC;
 
--- Recall the query that generated usernames for the employees from the last exercise.  Are there duplicate names?  YES
+-- Recall the query that generated usernames for the employees from the last exercise
 SELECT 
 	lower(
 		concat(
@@ -59,3 +61,26 @@ SELECT
  from employees
  group by username
  order by count(*) DESC;
+ 
+select count(*) from
+ (SELECT count(*) as username_count,
+	lower(
+		concat(
+			substring(first_name,1,1), 
+			substring(last_name,1,4),
+ 			'_', 
+ 			substring(birth_date,-5,2), 
+ 			substring(birth_date,3,2)
+ 		)
+ 	) as username,
+ 	count(*)
+ from employees
+ group by username
+ having username_count > 1)
+ as test;
+  
+ 
+
+
+
+
